67977b3e69f06d5a21970b070334b4bfc369506d
Ed Courtenay <ed@edcourtenay.co.uk>
2016-04-19 10:24:38 +0100

    Add partial view

---
+++ WebFilm/Controllers/FilmCountController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using WebFilm.Services;
+
+namespace WebFilm.Controllers
+{
+    public class FilmCountController : Controller
+    {
+        private readonly IFilmService _filmService;
+
+        public FilmCountController(IFilmService filmService)
+        {
+            _filmService = filmService;
+        }
+
+        [Route("filmcount")]
+        public PartialViewResult Index()
+        {
+            return PartialView("FilmCount", _filmService.GetFilms());
+        }
+    }
+}
